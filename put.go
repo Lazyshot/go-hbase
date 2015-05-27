563acd0a1ee8e2a147b28bdb0fed7c0c5c54bf04
Bryan Peterson <lazyshot@gmail.com>
2015-05-27 10:59:14 -0400

    switch to allowing put for multiple families in one request

--- put.go
+++ put.go
@@ -3,34 +3,52 @@ package hbase
 import (
 	pb "github.com/golang/protobuf/proto"
 	"github.com/lazyshot/go-hbase/proto"
+
+	"bytes"
 )
 
 type Put struct {
 	key        []byte
-	family     []byte
-	qualifiers [][]byte
-	values     [][]byte
-
-	response chan interface{}
+	families   [][]byte
+	qualifiers [][][]byte
+	values     [][][]byte
 }
 
-func CreateNewPut(key, family []byte) *Put {
+func CreateNewPut(key []byte) *Put {
 	return &Put{
 		key:        key,
-		family:     family,
-		qualifiers: make([][]byte, 0),
-		values:     make([][]byte, 0),
-		response:   make(chan interface{}, 1),
+		families:   make([][]byte, 0),
+		qualifiers: make([][][]byte, 0),
+		values:     make([][][]byte, 0),
 	}
 }
 
-func (this *Put) AddValue(column, value []byte) {
-	this.qualifiers = append(this.qualifiers, column)
-	this.values = append(this.values, value)
+func (this *Put) AddValue(family, column, value []byte) {
+	pos := this.posOfFamily(family)
+
+	if pos == -1 {
+		this.families = append(this.families, family)
+		this.qualifiers = append(this.qualifiers, make([][]byte, 0))
+		this.values = append(this.values, make([][]byte, 0))
+
+		pos = this.posOfFamily(family)
+	}
+
+	this.qualifiers[pos] = append(this.qualifiers[pos], column)
+	this.values[pos] = append(this.values[pos], value)
 }
 
-func (this *Put) AddStringValue(column, value string) {
-	this.AddValue([]byte(column), []byte(value))
+func (this *Put) AddStringValue(family, column, value string) {
+	this.AddValue([]byte(family), []byte(column), []byte(value))
+}
+
+func (this *Put) posOfFamily(family []byte) int {
+	for p, v := range this.families {
+		if bytes.Equal(family, v) {
+			return p
+		}
+	}
+	return -1
 }
 
 func (this *Put) toProto() pb.Message {
@@ -39,18 +57,20 @@ func (this *Put) toProto() pb.Message {
 		MutateType: proto.MutationProto_PUT.Enum(),
 	}
 
-	cv := &proto.MutationProto_ColumnValue{
-		Family: this.family,
-	}
+	for i, family := range this.families {
+		cv := &proto.MutationProto_ColumnValue{
+			Family: family,
+		}
 
-	for i, _ := range this.qualifiers {
-		cv.QualifierValue = append(cv.QualifierValue, &proto.MutationProto_ColumnValue_QualifierValue{
-			Qualifier: this.qualifiers[i],
-			Value:     this.values[i],
-		})
-	}
+		for j, _ := range this.qualifiers[i] {
+			cv.QualifierValue = append(cv.QualifierValue, &proto.MutationProto_ColumnValue_QualifierValue{
+				Qualifier: this.qualifiers[i][j],
+				Value:     this.values[i][j],
+			})
+		}
 
-	p.ColumnValue = append(p.ColumnValue, cv)
+		p.ColumnValue = append(p.ColumnValue, cv)
+	}
 
 	return p
 }
